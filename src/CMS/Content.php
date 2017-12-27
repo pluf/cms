8c4748f1d92755f722d958cb9e2fbad09b97899d
hadi <hadi@dpq.co.ir>
2017-12-28 01:41:57 +0330

    organize index of CMS_Content

--- src/CMS/Content.php
+++ src/CMS/Content.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of Pluf Framework, a simple PHP Application Framework.
  * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
@@ -31,132 +32,136 @@ class CMS_Content extends Pluf_Model
      *
      * @see Pluf_Model::init()
      */
-    function init ()
+    function init()
     {
         $this->_a['table'] = 'cms_content';
         $this->_a['cols'] = array(
-                // شناسه‌ها
-                'id' => array(
-                        'type' => 'Pluf_DB_Field_Sequence',
-                        'blank' => false,
-                        'verbose' => __('first name'),
-                        'help_text' => __('id'),
-                        'editable' => false
-                ),
-                // فیلدها
-                'name' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => true,
-                        'size' => 64,
-                        'verbose' => __('name'),
-                        'help_text' => __('content name'),
-                        'editable' => true
-                ),
-                'title' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => true,
-                        'size' => 250,
-                        'default' => 'no title',
-                        'verbose' => __('title'),
-                        'help_text' => __('content title'),
-                        'editable' => true
-                ),
-                'description' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => true,
-                        'size' => 250,
-                        'default' => 'auto created content',
-                        'verbose' => __('description'),
-                        'help_text' => __('content description'),
-                        'editable' => true
-                ),
-                'mime_type' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => false,
-                        'size' => 64,
-                        'default' => 'application/octet-stream',
-                        'verbose' => __('mime type'),
-                        'help_text' => __('content mime type'),
-                        'editable' => false
-                ),
-                'file_path' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => false,
-                        'size' => 250,
-                        'verbose' => __('file path'),
-                        'help_text' => __('content file path'),
-                        'editable' => false,
-                        'readable' => false
-                ),
-                'file_name' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => false,
-                        'size' => 250,
-                        'default' => 'unknown',
-                        'verbose' => __('file name'),
-                        'help_text' => __('content file name'),
-                        'editable' => false
-                ),
-                'file_size' => array(
-                        'type' => 'Pluf_DB_Field_Integer',
-                        'blank' => false,
-                        'default' => 'no title',
-                        'verbose' => __('file size'),
-                        'help_text' => __('content file size'),
-                        'editable' => false
-                ),
-                'downloads' => array(
-                        'type' => 'Pluf_DB_Field_Integer',
-                        'blank' => false,
-                        'default' => 0,
-                        'default' => 'no title',
-                        'verbose' => __('downloads'),
-                        'help_text' => __('content downloads number'),
-                        'editable' => false
-                ),
-                'creation_dtime' => array(
-                        'type' => 'Pluf_DB_Field_Datetime',
-                        'blank' => true,
-                        'verbose' => __('creation'),
-                        'help_text' => __('content creation time'),
-                        'editable' => false
-                ),
-                'modif_dtime' => array(
-                        'type' => 'Pluf_DB_Field_Datetime',
-                        'blank' => true,
-                        'verbose' => __('modification'),
-                        'help_text' => __('content modification time'),
-                        'editable' => false
-                ),
+            // شناسه‌ها
+            'id' => array(
+                'type' => 'Pluf_DB_Field_Sequence',
+                'blank' => false,
+                'verbose' => __('first name'),
+                'help_text' => __('id'),
+                'editable' => false
+            ),
+            // فیلدها
+            'name' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => true,
+                'size' => 64,
+                'unique' => true,
+                'verbose' => __('name'),
+                'help_text' => __('content name'),
+                'editable' => true
+            ),
+            'title' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => true,
+                'size' => 250,
+                'default' => 'no title',
+                'verbose' => __('title'),
+                'help_text' => __('content title'),
+                'editable' => true
+            ),
+            'description' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => true,
+                'size' => 250,
+                'default' => 'auto created content',
+                'verbose' => __('description'),
+                'help_text' => __('content description'),
+                'editable' => true
+            ),
+            'mime_type' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => false,
+                'size' => 64,
+                'default' => 'application/octet-stream',
+                'verbose' => __('mime type'),
+                'help_text' => __('content mime type'),
+                'editable' => false
+            ),
+            'file_path' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => false,
+                'size' => 250,
+                'verbose' => __('file path'),
+                'help_text' => __('content file path'),
+                'editable' => false,
+                'readable' => false
+            ),
+            'file_name' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => false,
+                'size' => 250,
+                'default' => 'unknown',
+                'verbose' => __('file name'),
+                'help_text' => __('content file name'),
+                'editable' => false
+            ),
+            'file_size' => array(
+                'type' => 'Pluf_DB_Field_Integer',
+                'blank' => false,
+                'default' => 'no title',
+                'verbose' => __('file size'),
+                'help_text' => __('content file size'),
+                'editable' => false
+            ),
+            'downloads' => array(
+                'type' => 'Pluf_DB_Field_Integer',
+                'blank' => false,
+                'default' => 0,
+                'default' => 'no title',
+                'verbose' => __('downloads'),
+                'help_text' => __('content downloads number'),
+                'editable' => false
+            ),
+            'creation_dtime' => array(
+                'type' => 'Pluf_DB_Field_Datetime',
+                'blank' => true,
+                'verbose' => __('creation'),
+                'help_text' => __('content creation time'),
+                'editable' => false
+            ),
+            'modif_dtime' => array(
+                'type' => 'Pluf_DB_Field_Datetime',
+                'blank' => true,
+                'verbose' => __('modification'),
+                'help_text' => __('content modification time'),
+                'editable' => false
+            )
         );
         
         $this->_a['idx'] = array(
-                'content_idx' => array(
-                        'col' => 'name',
-                        'type' => 'normal', // normal, unique, fulltext, spatial
-                        'index_type' => '', // hash, btree
-                        'index_option' => '',
-                        'algorithm_option' => '',
-                        'lock_option' => ''
-                ),
-                'content_mime_filter_idx' => array(
-                        'col' => 'mime_type',
-                        'type' => 'normal', // normal, unique, fulltext, spatial
-                        'index_type' => '', // hash, btree
-                        'index_option' => '',
-                        'algorithm_option' => '',
-                        'lock_option' => ''
-                )
+        // @Note: hadi - 1396-10: when define an attribute as 'unique => true', pluf automatically
+        // create an unique index for it (for example 'name' field here). So following codes are extra.
+//             'content_idx' => array(
+//                 'col' => 'name',
+//                 'type' => 'unique', // normal, unique, fulltext, spatial
+//                 'index_type' => '', // hash, btree
+//                 'index_option' => '',
+//                 'algorithm_option' => '',
+//                 'lock_option' => ''
+//             ),
+            'content_mime_filter_idx' => array(
+                'col' => 'mime_type',
+                'type' => 'normal', // normal, unique, fulltext, spatial
+                'index_type' => '', // hash, btree
+                'index_option' => '',
+                'algorithm_option' => '',
+                'lock_option' => ''
+            )
         );
     }
 
     /**
      * پیش ذخیره را انجام می‌دهد
      *
-     * @param boolean $create حالت
+     * @param boolean $create
+     *            حالت
      *            ساخت یا به روز رسانی را تعیین می‌کند
      */
-    function preSave ($create = false)
+    function preSave($create = false)
     {
         if ($this->id == '') {
             $this->creation_dtime = gmdate('Y-m-d H:i:s');
@@ -180,7 +185,7 @@ class CMS_Content extends Pluf_Model
      *
      * @see Pluf_Model::postSave()
      */
-    function postSave ($create = false)
+    function postSave($create = false)
     {
         //
     }
@@ -199,13 +204,13 @@ class CMS_Content extends Pluf_Model
             unlink($filename);
         }
     }
-    
+
     /**
      * مسیر کامل محتوی را تعیین می‌کند.
      *
      * @return string
      */
-    public function getAbsloutPath ()
+    public function getAbsloutPath()
     {
         return $this->file_path . '/' . $this->id;
     }
