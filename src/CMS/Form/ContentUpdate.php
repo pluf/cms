5bbf4c8cf15d55d4851a5ce838a9ac6500c12736
hadi <hadi@dpq.co.ir>
2018-09-10 21:46:57 +0430

    https://taskulu.com/a/project/5b2501d456ad66341e08ec40/tasks/5b52d11656ad6659831b8cb2

--- src/CMS/Form/ContentUpdate.php
+++ src/CMS/Form/ContentUpdate.php
@@ -72,6 +72,17 @@ class CMS_Form_ContentUpdate extends Pluf_Form_Model
             // Extract information of file
             $myFile = $this->data['file'];
             $model->file_name = $myFile['name'];
+            // set mime type if not defined
+            $mimeType = Pluf_FileUtil::getMimeType($model->file_name);
+            if(is_array($mimeType)){
+                $mimeType = $mimeType[0];
+            }
+            if(!array_key_exists('mime_type', $this->data)){
+                $model->mime_type = $mimeType;
+            }
+            if(!array_key_exists('media_type', $this->data)){
+                $model->media_type = $mimeType;
+            }
         }
         if ($commit) {
             $model->update();
