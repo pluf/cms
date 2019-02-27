e9dcb4df92944e34eb8c2505fb8908ce7d8181ae
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-02-27 21:53:43 +0330

    fix bugs in the relation

--- src/CMS/relations.php
+++ src/CMS/relations.php
@@ -19,13 +19,13 @@
 return array(
     'CMS_Content' => array(
         'relate_to' => array(
-            'User'
+            'User_Account'
         )
     ),
     'CMS_TermTaxonomy' => array(
         'relate_to' => array(
             'CMS_Term',
-            'User'
+            'User_Account'
         ),        
         'relate_to_many' => array(
             'CMS_Content'
