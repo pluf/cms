81af3ad645baad008179e620f45a66dac5905279
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-04-05 18:08:12 +0430

    Fix bug in SQL script

--- sql/V5.5.4.1__Cms_content_meta_is_added_to_the_data_model.sql
+++ sql/V5.5.4.1__Cms_content_meta_is_added_to_the_data_model.sql
@@ -6,10 +6,10 @@
 CREATE TABLE `cms_content_metas` (
   `id` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(255) NOT NULL DEFAULT '',
-  `value` longtext DEFAULT '',
+  `value` longtext,
   `content_id` mediumint(9) unsigned DEFAULT 0,
   `tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `tenant_foreignkey_idx` (`tenant`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
