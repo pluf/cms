3fa2636f73c0aaeab220ef26a3913634020a9857
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-05-04 14:04:25 +0430

    Add sql script to add unique index on slug of terms

---
+++ sql/V5.7.4.1__Cms_term_slug_index.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE `cms_term_metas` (
+  `id` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(256) NOT NULL DEFAULT '',
+  `value` longtext DEFAULT NULL,
+  `term_id` mediumint(9) unsigned DEFAULT 0,
+  `tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `key_unique_idx` (`tenant`,`key`),
+  KEY `content_id_foreignkey_idx` (`term_id`),
+  KEY `tenant_foreignkey_idx` (`tenant`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
