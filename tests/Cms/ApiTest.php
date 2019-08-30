3c92883213eb0818a194b5f38dda4ad18df42ab4
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2019-08-30 14:41:36 +0430

    Update PHPUnit version in the group

--- tests/Cms/ApiTest.php
+++ tests/Cms/ApiTest.php
@@ -30,7 +30,7 @@ class CmsApiTest extends TestCase
     /**
      * @before
      */
-    public function setUp ()
+    public function setUpTest ()
     {
         Pluf::start(__DIR__. '/../conf/config.php');
     }
