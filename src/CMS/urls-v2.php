d40cfcb17064f0b735b93b3b2e26ef17dc39af78
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2020-02-23 10:48:32 +0330

    add all php files in the folder `url` to API

--- src/CMS/urls-v2.php
+++ src/CMS/urls-v2.php
@@ -1,13 +1,11 @@
 <?php
-$paths = array(
-    'urls/content.php',
-    'urls/term-taxonomy.php',
-    'urls/term.php'
-);
+$paths = array_filter(glob(__DIR__ . '/urls/*.php'), function ($file) {
+    return is_file($file);
+});
 
 $cmsApi = array();
 
-foreach ($paths as $path){
+foreach ($paths as $path) {
     $myApi = include $path;
     $cmsApi = array_merge($cmsApi, $myApi);
 }
