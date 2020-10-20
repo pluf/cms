128246e0f80117ad855fd6abde7ef807d510582c
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2020-10-20 13:52:19 +0330

    !!

--- sql/V5.7.4.2__Cms_term_meta_is_added.sql
+++ sql/V5.7.4.2__Cms_term_meta_is_added.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `cms_term_metas` (
   `id` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(256) NOT NULL DEFAULT '',
-  `value` longtext DEFAULT NULL,
+  `value` text DEFAULT NULL,
   `term_id` mediumint(9) unsigned DEFAULT 0,
   `tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
